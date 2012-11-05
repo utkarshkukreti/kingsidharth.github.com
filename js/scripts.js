cc6f5c26558443e2b5787ac7b7896fa675f38118
King Sidharth <king@kingsidharth.com>
2012-11-05 17:42:07 +0530

    Fix the background to fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+/*
+ * INDEX
+ * 1. Sets minimum content height to full browser height.
+ */
 
 
+// 1. Sets minimum content height to full browser height.
 $(function() {
   $("#content_box").css("min-height", $(window).height() ); 
   //$("#logo").css("height", $("#logo").css('width') );
 }); 
+
+//2. 
+var os = navigator.platform;
