6abcb179f0c72167cfe32ac0a580eea0295cb00f
King Sidharth <king@kingsidharth.com>
2012-11-05 12:57:11 +0530

    Fix logo.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@
 
 $(function() {
   $("#content_box").css("min-height", $(window).height() ); 
-  $("#logo").css("height", $("#logo").css('width') );
+  //$("#logo").css("height", $("#logo").css('width') );
 }); 
